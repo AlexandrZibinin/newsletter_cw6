d6ea5e8562548cc3c752af6436c74b69d2448c87
Alexandr Zibinin <zibinin.chel@gmail.com>
2024-07-10 01:50:30 +0300

    остался блог

--- newsletter/urls.py
+++ newsletter/urls.py
@@ -1,3 +1,5 @@
+from django.views.decorators.cache import cache_page
+
 from newsletter.apps import NewsletterConfig
 from django.urls import path
 
@@ -13,8 +15,8 @@ from newsletter.views import (
 app_name = NewsletterConfig.name
 
 urlpatterns = [
-    path("", index, name="index"),
-    path("newsletter/list/", NewsletterListView.as_view(), name="list"),
+    path("", cache_page(60)(index), name="index"),
+    path("newsletter/list/", cache_page(60)(NewsletterListView.as_view()), name="list"),
     path("newsletter/create", NewsletterCreateView.as_view(), name="create"),
     path("newsletter/list/<int:pk>/", NewsletterDetailView.as_view(), name="detail"),
     path("newsletter/<int:pk>/update", NewsletterUpdateView.as_view(), name="update"),
