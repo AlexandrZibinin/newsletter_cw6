cca3293bc63afc8b16ae08c47f5114c3854507b0
Alexandr Zibinin <zibinin.chel@gmail.com>
2024-07-10 00:57:50 +0300

    остался блог

--- blog/urls.py
+++ blog/urls.py
@@ -1,20 +1,10 @@
 from django.urls import path
 
 from blog.apps import BlogConfig
-from blog.views import (
-    BlogListView,
-    BlogCreateView,
-    BlogDetailView,
-    BlogUpdateView,
-    BlogDeleteView,
-)
+from blog.views import BlogView
 
 app_name = BlogConfig.name
 
 urlpatterns = [
-    path("", BlogListView.as_view(), name="blog_list"),
-    path("create/", BlogCreateView.as_view(), name="blog_create"),
-    path("view/<int:pk>/", BlogDetailView.as_view(), name="blog_view"),
-    path("edit/<int:pk>/", BlogUpdateView.as_view(), name="blog_edit"),
-    path("delete/<int:pk>/", BlogDeleteView.as_view(), name="blog_delete"),
+    path("index/", BlogView.as_view(), name="index"),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,5 @@
 from django.urls import reverse_lazy
 from django.views.generic import (
-    ListView,
-    CreateView,
-    UpdateView,
-    DeleteView,
-    DetailView,
     TemplateView,
 )
 from pytils.translit import slugify
@@ -12,66 +7,7 @@ from pytils.translit import slugify
 from blog.models import Blog
 
 
-class BaseView(TemplateView):
-    template_name = "blog/base_view.html"
-
-
-class BlogListView(ListView):
-    model = Blog
-    success_url = reverse_lazy("blog:blog_list")
-
-    def get_queryset(self, *args, **kwargs):
-        queryset = super().get_queryset(*args, **kwargs)
-        queryset = queryset.filter(public=True)
-        return queryset
-
-
-class BlogDetailView(DetailView):
+class BlogView(TemplateView):
     model = Blog
 
-    def get_object(self, queryset=None):
-        self.object = super().get_object(queryset)
-        self.object.views += 1
-        self.object.save()
-        return self.object
-
-
-class BlogCreateView(CreateView):
-    model = Blog
-    fields = (
-        "title",
-        "text",
-        "image",
-    )
-    success_url = reverse_lazy("blog:blog_list")
-
-    def form_valid(self, form):
-        if form.is_valid():
-            new_blog = form.save()
-            new_blog.slug = slugify(new_blog.title)
-            new_blog.save()
-        return super().form_valid(form)
-
-
-class BlogUpdateView(UpdateView):
-    model = Blog
-    fields = (
-        "title",
-        "text",
-        "image",
-    )
-
-    def get_success_url(self):
-        return reverse_lazy("blog:blog_view", kwargs={"pk": self.object.pk})
-
-    def form_valid(self, form):
-        if form.is_valid():
-            new_blog = form.save()
-            new_blog.slug = slugify(new_blog.title)
-            new_blog.save()
-        return super().form_valid(form)
-
-
-class BlogDeleteView(DeleteView):
-    model = Blog
-    success_url = reverse_lazy("blog:blog_list")
+    template_name = "blog/index.html"
